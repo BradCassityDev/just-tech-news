635e81848a8717dc712db671b611f1a542fd8fb0
Brad Cassity <bradley.cassity@gmail.com>
2020-10-28 13:02:42 -0600

    Added sessions for authentication

--- server.js
+++ server.js
@@ -4,14 +4,35 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const path = require('path');
 
-const hbs = exphbs.create({});
 
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Template Engine
 app.engine('handlebars', hbs.engine);
