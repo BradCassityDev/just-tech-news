c6749e0425b5c6ae6b61b03601031b2a5d1d0f55
Brad Cassity <bradley.cassity@gmail.com>
2020-10-28 15:22:03 -0600

    Added single post page. Created functionality to allow users to upvote and leave comments. Dynamically show content based on session status

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+// Get comments
 router.get('/', (req, res) => {
     Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
@@ -10,19 +11,25 @@ router.get('/', (req, res) => {
     });
 });
 
+// Post a comment
 router.post('/', (req, res) => {
+  // check the session
+  if (req.session) {
     Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-      })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 });
 
+// Delete a comment
 router.delete('/:id', (req, res) => {
     Comment.destroy({
         where: {
