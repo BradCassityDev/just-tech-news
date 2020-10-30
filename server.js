ba23fc44e2b5b2cc4420e0a9c00c7d8b9123e726
Brad Cassity <bradley.cassity@gmail.com>
2020-10-30 17:06:56 -0600

    added helper functions to clean up text

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const helpers = require('./utils/helpers');
 
-
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
